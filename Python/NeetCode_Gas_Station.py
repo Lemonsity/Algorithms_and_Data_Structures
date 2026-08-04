61cf195dafa55bcb7c3df0aefff0c5e6b44d5256
M <38927629+YZmarkS@users.noreply.github.com>
2026-08-04 19:51:17 -0400

    Update

--- Python/NeetCode_Gas_Station.py
+++ Python/NeetCode_Gas_Station.py
@@ -16,3 +16,24 @@ def canCompleteCircuit(gas: List[int], cost: List[int]) -> int:
             ans = start
             break
     return ans
+
+def canComplete(gas: List[int], cost: List[int]) -> int:
+    length = len(gas)
+    end, start = 0, length - 1
+    tank = gas[start]
+    while end < start:
+        print(tank, start, end)
+        if cost[end - 1] <= tank:
+            print("one")
+            tank -= cost[end - 1]
+            tank += gas[end]
+            end += 1
+        elif tank < cost[end - 1]:
+            print("two")
+            start -= 1
+            tank += gas[start]
+            tank -= cost[start]
+    print(tank, start, end)
+    if cost[end - 1] <= tank:
+        return start
+    return -1
